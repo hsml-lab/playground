efd9ea419e30749b727215140e896bdb62b2e757
Shinigami92 <chrissi92@hotmail.de>
2026-04-11 11:15:06 +0200

    chore(deps): migrate radix-vue to reka-ui v2

--- src/components/ToggleSwitch.vue
+++ src/components/ToggleSwitch.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { SwitchRoot, SwitchThumb } from 'radix-vue';
+import { SwitchRoot, SwitchThumb } from 'reka-ui';
 
 const model = defineModel<boolean>({ required: true });
 </script>
 
 <template lang="hsml">
 SwitchRoot(
-  v-model:checked="model"
+  v-model="model"
   class="peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-gray-300 dark:data-[state=unchecked]:bg-gray-600"
 )
   SwitchThumb(
